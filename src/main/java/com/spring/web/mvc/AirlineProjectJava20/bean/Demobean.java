c3f6227fd41c2b9b3794fbdb1a7f1af80903a725
AshwinS12 <ashwin.vtalent@gmail.com>
2022-10-28 15:58:37 +0530

    Created demo bean

---
+++ src/main/java/com/spring/web/mvc/AirlineProjectJava20/bean/Demobean.java
@@ -0,0 +1,41 @@
+package com.spring.web.mvc.AirlineProjectJava20.bean;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity 
+@Table(name = "airline")
+public class Demobean {
+@Id  @GeneratedValue
+@Column(name="num")
+	private int num;
+@Column(name="name")
+	private String name;
+@Column(name="flag")
+	private boolean flag;
+
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public boolean isFlag() {
+		return flag;
+	}
+	public void setFlag(boolean flag) {
+		this.flag = flag;
+	}
+	
+	
+	
+}
--- src/main/java/com/spring/web/mvc/AirlineProjectJava20/bean/bean.java
+++
@@ -1,5 +0,0 @@
-package com.spring.web.mvc.AirlineProjectJava20.bean;
-//
-public class bean {
-//
-}
