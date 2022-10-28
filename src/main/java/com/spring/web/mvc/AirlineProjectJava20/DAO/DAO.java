f5d4bb09c134ae2774a415a98afea8fef6dbebd0
GeethaRaniN <geetha.vtalent@gmail.com>
2022-10-28 17:13:19 +0530

    DAO started

---
+++ src/main/java/com/spring/web/mvc/AirlineProjectJava20/DAO/DAO.java
@@ -0,0 +1,9 @@
+package com.spring.web.mvc.AirlineProjectJava20.DAO;
+
+import com.spring.web.mvc.AirlineProjectJava20.bean.Loginbean;
+
+public interface DAO {
+	public String LoginDetails(Loginbean loginbean); 
+		
+	
+}
---
+++ src/main/java/com/spring/web/mvc/AirlineProjectJava20/DAO/DAOImplementation.java
@@ -0,0 +1,5 @@
+package com.spring.web.mvc.AirlineProjectJava20.DAO;
+
+public class DAOImplementation implements DAO {
+
+}
