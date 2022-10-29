1af1d2aa0fdc8940133cf52e40106e9704310b0c
AshwinS12 <ashwin.vtalent@gmail.com>
2022-10-29 10:31:01 +0530

    controller

---
+++ src/main/java/com/spring/web/mvc/AirlineProjectJava20/controller/LoginController.java
@@ -0,0 +1,19 @@
+package com.spring.web.mvc.AirlineProjectJava20.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+@RequestMapping(value = "registeringcustomer")
+public String OpenRegisterationPage() {
+	String views = "NewFile";
+	return views;
+}
+
+@RequestMapping(value = "Logincustomer")
+public String OpenLoginPage() {
+	String views = "NewFile1";
+	return views;
+}
+}
---
+++ src/main/java/com/spring/web/mvc/AirlineProjectJava20/controller/ServiceController.java
@@ -0,0 +1,25 @@
+package com.spring.web.mvc.AirlineProjectJava20.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.spring.web.mvc.AirlineProjectJava20.bean.Loginbean;
+
+@Controller
+public class ServiceController {
+@RequestMapping(value = "UserLoginRequest",method = RequestMethod.POST)
+public ModelAndView readLogin(@ModelAttribute("UserObj")Loginbean lb) {
+	int result = 0;
+	ModelAndView mav = new ModelAndView();
+	//result = daoclass . insertstatement(lb);
+	mav.setViewName("home");
+	mav.addObject("insertresponsevalue",result);
+	
+	
+	return mav;
+	
+}
+}
