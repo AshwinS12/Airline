6f78401a5cdfdfaf63848dae7daa1046244f1072
GeethaRaniN <geetha.vtalent@gmail.com>
2022-10-29 17:08:37 +0530

    Merge branch 'master' of https://github.com/AshwinS12/Airline.git into Geetha

--- src/main/java/com/spring/web/mvc/AirlineProjectJava20/DAO/DAOImplementation.java
+++ src/main/java/com/spring/web/mvc/AirlineProjectJava20/DAO/DAOImplementation.java
@@ -1,46 +1,68 @@
 package com.spring.web.mvc.AirlineProjectJava20.DAO;
 
-import org.hibernate.Query;
 import org.hibernate.Session;
+
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.spring.web.mvc.AirlineProjectJava20.bean.Loginbean;
 
-@SuppressWarnings("deprecation")
+
 public class DAOImplementation implements DAO {
 	@Autowired
 	SessionFactory sessionfactory;
-
+//LoginPage
 	@Override
+	public boolean logindetails(String name, String password) {
+		Session session =sessionfactory.openSession();
+		Query query=session.createQuery("from airline emp where Loginbean.name='"+name+"' and Loginbean.password="+password);
+		boolean login=false;
+		Loginbean beanl = (Loginbean) query.uniqueResult();
+		if(beanl == null) {
+			login = false;
+		}else {
+			login = true;
+		}
 
-	public String SignUp(Loginbean loginbean) {
-		String result = "";
-		Session session = sessionfactory.openSession();
-		Object obj = session.save(loginbean);
-		Transaction trans = session.beginTransaction();
+		return login;
+		
+	}
+//Registration
+	public String SignUp(Loginbean LB) {
+		String result="";
+		Session session=sessionfactory.openSession();
+		
+		Object obj=session.save(LB);
+		Transaction trans=session.beginTransaction();
 		trans.commit();
-		if (obj != null) {
-			result = (String) obj;
+		if(obj!=null) {
+			result=(String) obj;
 		}
 		return result;
 	}
-
-	public boolean logindetails(String name, String password) {
-		boolean bean = false;
-		Session session = sessionfactory.openSession();
-		@SuppressWarnings("rawtypes")
-		Query query = session.createQuery(
-				"from airline Loginbean where Loginbean.name='" + name + "' and Loginbean.password=" + password);
-		@SuppressWarnings("deprecation")
-		Loginbean login = (Loginbean) query.uniqueResult();
-		if (login == null) {
-			bean = false;
-		} else {
-			bean = true;
+//Search Ticket
+	public String Searchticket(Loginbean LB) {
+		
+		String result="";
+		Session session=sessionfactory.openSession();
+		Object obj=session.createQuery("from airline TicketBooking where TicketBooking.TicketNumber='"+LB+"'");
+		if(obj!=null) {
+			result=(String) obj;
 		}
-		return bean;
+		return result;
 	}
-
+//Cancel Ticket
+	public String Cancelticket(String ticketnumber) {
+		String result="";
+		Session session=sessionfactory.openSession();
+		Object obj = session.createQuery("Upadte airline TicketBooking set TicketBooking.Status=Cancel where TicketBoking.TicketNumber='"+ticketnumber+"'");
+		if(obj!=null) {
+			result=(String) obj;
+		}
+		return result;
+	}
 }
+
+
